5a13faef64d68c0aaccfb76f0eb08135a2a89d75
Anna Claire <annaclaire@MBP-von-Anna.fritz.box>
2024-09-19 14:56:26 +0200

    more configuration + swagger

---
+++ src/swagger.ts
@@ -0,0 +1,26 @@
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import { Express } from 'express';
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Express API with Swagger',
+      version: '1.0.0',
+      description: 'A simple CRUD API application with Swagger UI documentation',
+    },
+    servers: [
+      {
+        url: 'http://localhost:8000', // URL of your API
+      },
+    ],
+  },
+  apis: ['./src/routes/*.ts'], // Path to your route files for documentation
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+export const setupSwagger = (app: Express) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
